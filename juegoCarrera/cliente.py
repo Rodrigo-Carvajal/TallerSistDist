8c2c490c13baa8945ceb68f80d52035dbe632120
Rodrigo-Carvajal <rodrigo.carvajal.ramos@gmail.com>
2022-12-03 18:54:55 -0300

    commit

--- juegoCarrera/cliente.py
+++ juegoCarrera/cliente.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 import threading
 import socket
 import time
@@ -12,10 +13,10 @@ DIRECCON = (SERVER,PUERTO)
 socketCon = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Creación de la ventana
-ancho = 500
-alto = 500
+ancho = 600
+alto = 600
 ventana = pygame.display.set_mode( (ancho, alto) )
-pygame.display.set_caption("|   T a l l e r    d e    S o c k e t s📨")
+pygame.display.set_caption("|   T a l l e r    d e    S o c k e t s ~ S i s t e m a s    D i s t r i b u i d o s📨")
 icono = pygame.image.load('icono.png')
 ico = pygame.transform.scale(icono, (32,32) )
 pygame.display.set_icon(icono)
@@ -29,7 +30,7 @@ class Jugador():
         self.alto = alto
         self.color = color
         self.rect = (x,y,ancho,alto)
-        self.vel = 7
+        self.vel = 5
     
     def draw(self, ventana):
         pygame.draw.rect(ventana, self.color, self.rect)
@@ -39,7 +40,19 @@ class Jugador():
 
         if keys[pygame.K_LEFT]:
             self.x -= self.vel
+        
+        if self.x < 0:
+            self.x = 0
+        
+        if self.x > ancho-51:
+            self.x = ancho-50
 
+        if self.y < 0:
+            self.y = 0      
+
+        if self.y > ancho-51:
+            self.y = ancho-50              
+        
         if keys[pygame.K_RIGHT]:
             self.x += self.vel
 
@@ -61,7 +74,8 @@ def main():
     socketCon.connect(DIRECCON)
     conexion = socketCon.recv(1024)
     run = True
-    p = Jugador(50,50,100,100,(10,230,230))
+    randcolor = list(np.random.choice(range(256), size=3))
+    player = Jugador(200,200,50,50, randcolor)
     clock = pygame.time.Clock()
 
     while run:
@@ -72,8 +86,8 @@ def main():
                 pygame.quit()
                 socketCon.send(exit)
         
-        p.move()
-        redibujarVentana(ventana, p)
+        player.move()
+        redibujarVentana(ventana, player)
 
 main()
 
